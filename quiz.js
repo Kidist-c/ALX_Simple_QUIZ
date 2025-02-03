b635b842b948dfe803830b16f9154915a9ef6f38
kidistMulualem <mulualemkidist4@gmail.com>
2025-02-03 23:44:46 +0900

    applied javascrift

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer(){
+    const correctAnswer='4';
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    const feedBacked=document.getElementById('feedback');
+    if(userAnswer==correctAnswer){
+        feedBacked.textcontent="correct!Welldone.";
+    }else{
+        feedBacked.textcontent="That's incorrect.Try again!";
+    }
+
+}
+const submitButton=document.getElementById("submit-Answer");
+submitButton.addEventListener("click",checkAnswer);
