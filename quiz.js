fc699e122f14747ddbee7aa1fd656d5f52776556
kidistMulualem <mulualemkidist4@gmail.com>
2025-02-03 23:50:00 +0900

    make achange

--- quiz.js
+++ quiz.js
@@ -2,7 +2,7 @@ function checkAnswer(){
     const correctAnswer='4';
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
     const feedBacked=document.getElementById('feedback');
-    if(userAnswer==correctAnswer){
+    if(userAnswer===correctAnswer){
         feedBacked.textcontent="correct!Welldone.";
     }else{
         feedBacked.textcontent="That's incorrect.Try again!";
